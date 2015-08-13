7adf687d4144d1d60e5b9b96103c0939a33d4ebd
refluster <refluster@gmail.com>
2015-08-13 12:44:03 +0900

    create sqlite db for photoManager

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-
+var fs = require('fs');
+var sqlite3 = require('sqlite3').verbose();
+/*
 app.get('/', function (req, res) {
 	res.send('Hello World! ' + req.query.myquery);
 });
@@ -11,25 +13,47 @@ var server = app.listen(3000, function () {
 	
 	console.log('Example app listening at http://%s:%s', host, port);
 });
-
-var sqlite3 = require('sqlite3').verbose();
-
-var db = new sqlite3.Database('./db.bin');
-//var db = new sqlite3.Database(':memory:');
- 
-db.serialize(function() {
-	db.run("CREATE TABLE lorem (info TEXT)");
+*/
+var Database = function() {
+	this.dbFileName = "photo_manager.db";
 	
-	var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-	for (var i = 0; i < 10; i++) {
-		stmt.run("Ipsum " + i);
+
+	if (fs.existsSync(this.dbFileName)) {
+		this.db = new sqlite3.Database(this.dbFileName);
+		
+		this.db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+			//			console.log(row.id + ": " + row.info);
+			console.log(row);
+		});
+		
+		this.db.close();
+		console.log('exist');
+
+	} else {
+		// first time
+		console.log('none exist');
+		this.db = new sqlite3.Database(this.dbFileName);
+		this.db.serialize(function() {
+			this.db.run("CREATE TABLE photoManager (file TEXT, date TEXT)");
+
+			var stmt = this.db.prepare("INSERT INTO photoManager VALUES ($file, $date)");
+			for (var i = 0; i < 10; i++) {
+				//stmt.run("Ipsum " + i);
+				var date;
+				if (i > 5) {
+					date = '2015-08-12';
+				} else {
+					date = '2015-08-13';
+				}
+				stmt.run({$file: "picture" + i + ".jpg", $date: date});
+			}
+			stmt.finalize();
+		}.bind(this));
+
+		this.db.close();
 	}
-	stmt.finalize();
 	
-	db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-		console.log(row.id + ": " + row.info);
-	});
-});
+};
 
-db.close();
+db = new Database();
 
