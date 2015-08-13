55afb97942b733a642257830dc85e133fe96d725
refluster <refluster@gmail.com>
2015-08-13 13:52:16 +0900

    sqlite db insert and select from app

--- app.js
+++ app.js
@@ -2,58 +2,66 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var sqlite3 = require('sqlite3').verbose();
-/*
-app.get('/', function (req, res) {
-	res.send('Hello World! ' + req.query.myquery);
-});
 
-var server = app.listen(3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-	
-	console.log('Example app listening at http://%s:%s', host, port);
-});
-*/
+
 var Database = function() {
-	this.dbFileName = "photo_manager.db";
-	
+	//this.dbFileName = 'photo_manager.db';
+	this.dbFileName = ':memory:';
 
-	if (fs.existsSync(this.dbFileName)) {
-		this.db = new sqlite3.Database(this.dbFileName);
-		
-		this.db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-			//			console.log(row.id + ": " + row.info);
-			console.log(row);
-		});
-		
-		this.db.close();
-		console.log('exist');
-
-	} else {
-		// first time
-		console.log('none exist');
-		this.db = new sqlite3.Database(this.dbFileName);
-		this.db.serialize(function() {
-			this.db.run("CREATE TABLE photoManager (file TEXT, date TEXT)");
-
-			var stmt = this.db.prepare("INSERT INTO photoManager VALUES ($file, $date)");
-			for (var i = 0; i < 10; i++) {
-				//stmt.run("Ipsum " + i);
-				var date;
-				if (i > 5) {
-					date = '2015-08-12';
-				} else {
-					date = '2015-08-13';
-				}
-				stmt.run({$file: "picture" + i + ".jpg", $date: date});
-			}
-			stmt.finalize();
-		}.bind(this));
-
-		this.db.close();
+	// create db if not exist
+	/*
+	if (! fs.existsSync(this.dbFileName)) {
+		var db = new sqlite3.Database(this.dbFileName);
+		db.run("CREATE TABLE photoManager (fileOrig TEXT, fileLarge TEXT, fileThumb TEXT, date TEXT)");
+		db.close();
 	}
-	
+	*/
+	this.db = new sqlite3.Database(this.dbFileName);
+	this.db.exec("CREATE TABLE photoManager (fileOrig TEXT, fileLarge TEXT, fileThumb TEXT, date TEXT)");
+};
+Database.prototype.insert = function(fileOrig, fileLarge, fileThumb, date) {
+	console.log(this.db);
+	console.log({$fileOrig: fileOrig, $fileLarge: fileLarge, $fileThumb: fileThumb, $date: date});
+
+	this.db.run("INSERT INTO photoManager VALUES ($fileOrig, $fileLarge, $fileThumb, $date)",
+				 {$fileOrig: fileOrig, $fileLarge: fileLarge, $fileThumb: fileThumb, $date: date});
+};
+Database.prototype.getByDate = function(date) {
+	var data = [];
+
+	this.db.each("SELECT * FROM photoManager where date = $date", {$date: date}, function(err, row) {
+		console.log('- ' + row.fileOrig);
+	});
 };
 
-db = new Database();
+var Apl = function() {
+	var count = 0;
+	var db = new Database();
 
+	app.get('/', function (req, res) {
+		switch(count) {
+		case 0:
+			db.insert('f.jpg', 'fl.jpg', 'fthumb.jpg', '2015-08-30');
+			db.insert('f.jpg', 'fl.jpg', 'fthumb.jpg', '2015-08-30');
+			db.insert('f.jpg', 'fl.jpg', 'fthumb.jpg', '2015-08-31');
+			break;
+		case 1:
+			db.getByDate('2015-08-31');
+			break;
+		case 2:
+			db.getByDate('2015-08-30');
+			break;
+		}
+		++ count;
+
+		res.send('Hello World! ' + req.query.myquery);
+	});
+	
+	var server = app.listen(3000, function () {
+		var host = server.address().address;
+		var port = server.address().port;
+		console.log('Example app listening at http://%s:%s', host, port);
+	});
+};
+
+new Apl();
